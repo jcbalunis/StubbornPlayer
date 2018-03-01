4643e9fefdef8043ea8ad3d7f88976f780fc489a
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-28 17:35:21 -0800

    The automatic game

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include "SecretDoor/SecretDoor.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+        int games, wins;
+
+
+        cout<< "How many games do you want to play? \n"; 
+        cin>> games;
+
+        for( int i = 0; i < games; i++)
+        {   
+                game.newGame();
+                game.guessDoorC();
+                game.guessDoorC();
+                game.isWinner();
+                if (game.isWinner())
+                { 
+                        wins ++;
+
+
+                }
+        }       
+
+        cout<< "you have a total of "<<wins<<" wins. \n";
+
+
+
+
+
+
+
+        return 0;
 }
